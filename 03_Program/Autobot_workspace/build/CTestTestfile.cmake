6a8fd7422c382f63812c18fc628dcec19085c0fb
xin-Dream <sduzgx@foxmail.com>
2021-11-10 11:19:35 +0800

    make workspace on jetsonTX2

---
+++ 03_Program/Autobot_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dream/data/Autobot/03_Program/Autobot_workspace/src
+# Build directory: /home/dream/data/Autobot/03_Program/Autobot_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ 03_Program/Autobot_workspace/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/dream/data/Autobot/03_Program/Autobot_workspace/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
