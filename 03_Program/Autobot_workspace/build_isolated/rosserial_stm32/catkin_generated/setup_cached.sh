ed44e357c2c4ff65b658867f3360ced14500ab14
xin-Dream <sduzgx@foxmail.com>
2021-12-16 17:48:23 +0800

    add rosserial_stm32

--- 03_Program/Autobot_workspace/build_isolated/rosserial_stm32/catkin_generated/setup_cached.sh
+++ 03_Program/Autobot_workspace/build_isolated/rosserial_stm32/catkin_generated/setup_cached.sh
@@ -8,5 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/dream/01-data/005_Autobot/Autobot/03_Program/Autobot_workspace/devel_isolated/rosserial_stm32:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/dream/01-data/005_Autobot/Autobot/03_Program/Autobot_workspace/devel_isolated/rosserial_stm32/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/dream/01-data/005_Autobot/Autobot/03_Program/Autobot_workspace/devel_isolated/rosserial_stm32/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/dream/01-data/005_Autobot/Autobot/03_Program/Autobot_workspace/devel_isolated/rosserial_stm32/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/dream/01-data/005_Autobot/Autobot/03_Program/Autobot_workspace/devel_isolated/rosserial_stm32/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/dream/01-data/005_Autobot/Autobot/03_Program/Autobot_workspace/src/rosserial_stm32:$ROS_PACKAGE_PATH"
